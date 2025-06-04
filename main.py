08c299a33f8363df2f8cfcc41af17d56eb9b90af
George <gpolonifis88@gmail.com>
2025-06-04 23:56:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from faker import Faker
+
+fake = Faker()
+
+print("Name:", fake.name())
+print("Address:", fake.address())
+print("Country:", fake.country())
+print("Email:", fake.email())
+print("Phone Number:", fake.phone_number())
+print("Birth Date:", fake.date_of_birth())
+print("Job:", fake.job())
+print("Company:", fake.company())
+print("username:", fake.user_name())
+print("Text:", fake.text())
+print("Phone Number:", fake.phone_number())
+print("Adress:", fake.street_address())
